6eec5b946bb378e12eff3fac757529ea9b85a4c1
asim samuel <asim.samuel1@gmail.com>
2019-08-05 16:19:30 -0400

    set up app with UICollectionVIew

---
+++ Names to Faces/PersonalCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalCell.swift
+//  Names to Faces
+//
+//  Created by Asim on 8/5/19.
+//  Copyright © 2019 Asim Samuel. All rights reserved.
+//
+
+import UIKit
+
+class PersonalCell: UICollectionViewCell {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    @IBOutlet var name: UILabel!
+    
+}
